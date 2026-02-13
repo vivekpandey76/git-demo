f70d408e78f70429b7f753ea293242cb244b2e18
Vivekpandey76 <vivekpandey22982@gmail.com>
2026-02-13 14:58:46 +0530

    index.html

--- server.js
+++ server.js
@@ -1,1 +1,9 @@
-console.log("how to upload code to github")
+setTimeout(() => {
+  console.log("Macrotask (2s)");
+}, 0);
+
+Promise.resolve().then(() => {
+  const start = Date.now();
+  while (Date.now() - start < 10000) {} // 10 sec block
+  console.log("Microtask (10s)");
+});
